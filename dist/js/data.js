e26a266f36152929d57c61a0340ec7443cfd8ea3
rarean <db.robot@yaoo.com>
2020-04-03 17:07:46 -0500

    init

---
+++ dist/js/data.js
@@ -0,0 +1,32 @@
+const main = {
+  name: 'Aaron Perez',
+  mail: 'db.robot@yahoo.com',
+  img: './dist/img/selfy1.jpg',
+  role: ['Software Engineering Manager'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://rarean.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/rarean'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/aaronperez/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'db.robot@yahoo.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: 'https://github.com/rarean?tab=repositories' }
+  ]
+};
